4dbb6672847758b95ec06c9ec78e1a2b7820c703
leejh <programmer.leejh0915@gmail.com>
2023-01-22 14:18:18 +0900

    [DWS-1] 작업 초기환경 세팅 완료

---
+++ main.py
@@ -0,0 +1,30 @@
+import cv2
+
+# main.py
+
+# 라이브러리 import
+# StreamingResponse를 가져와야함
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+import uvicorn
+
+# cv2 모듈 import
+from Camera import get_stream_video
+
+# FastAPI객체 생성
+app = FastAPI()
+
+# openCV에서 이미지 불러오는 함수
+def video_streaming():
+    return get_stream_video()
+
+# 스트리밍 경로를 /video 경로로 설정.
+@app.get("/video")
+def video():
+    # StringResponse함수를 return하고,
+    # 인자로 OpenCV에서 가져온 "바이트"이미지와 type을 명시
+    return StreamingResponse(video_streaming(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=2121)
